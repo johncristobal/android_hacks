e860ca36d53337dc07c80de2b5f28c7f1349babf
johhcristobal <johnalexiscristobaljimenez6@gmail.com>
2026-06-12 21:55:08 -0600

    split buttons

--- app/src/main/java/com/android2025/tips/MainActivity.kt
+++ app/src/main/java/com/android2025/tips/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.tooling.preview.Preview
 import com.android2025.tips.ui.theme.HacksTheme
 import com.android2025.tips.utils.material3.SplitButtons
@@ -25,18 +28,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HacksTheme {
-                SplitButtons()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(it),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        SplitButtons()
+                    }
+                }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/android2025/tips/utils/material3/SplitButtons.kt
+++ app/src/main/java/com/android2025/tips/utils/material3/SplitButtons.kt
@@ -1,14 +1,27 @@
 package com.android2025.tips.utils.material3
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.SplitButtonLayout
 import androidx.compose.material3.SplitButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import com.android2025.tips.R
+import kotlin.math.tan
 
 @Composable
 fun SplitButtons(modifier: Modifier = Modifier) {
@@ -21,15 +34,54 @@ fun SplitButtons(modifier: Modifier = Modifier) {
             ) {
                 Icon(
                     imageVector = ImageVector.vectorResource(R.drawable.outline_edit_24),
-                    contentDescription = null
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(SplitButtonDefaults.LeadingIconSize)
                 )
+                Spacer(modifier = Modifier.width(8.dp))
                 Text(
                     text = "Edit"
                 )
             }
         },
         trailingButton = {
-
+            var checked by remember {
+                mutableStateOf(false)
+            }
+            val rotation by animateFloatAsState(
+                targetValue = if (checked) 180f else 0f
+            )
+            
+            SplitButtonDefaults.TrailingButton(
+                checked = checked,
+                onCheckedChange = {
+                    checked = it
+                }
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.outline_arrow_drop_down_24),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .graphicsLayer {
+                            rotationZ = rotation
+                        }
+                )
+            }
+            DropdownMenu(
+                expanded = checked,
+                onDismissRequest = {
+                    checked = false
+                }
+            ) {
+                for(i in 1..3) {
+                    DropdownMenuItem(
+                        text = { Text("Item $i") },
+                        onClick = {
+                            checked = false
+                        }
+                    )
+                }
+            }
         }
     )
 }
